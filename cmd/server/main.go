63caba2ad836a0e8cd4f0afb8cc545c751354c14
F4c3hugg3r <Lennard.Dubhorn@Student.htw-berlin.de>
2025-06-11 15:34:17 +0200

    added to server: handleRegistry function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 )
 
 type Client struct {
-	name     string
+	Name     string
 	clientId string
-	Message  chan string
 }
 
 type Message struct {
@@ -19,32 +20,83 @@ type Message struct {
 }
 
 var (
-	broadcast chan Message
-
-	clients []Client
-	mu      sync.Mutex
+	broadcast chan Message      = make(chan Message)
+	clients   map[string]Client = make(map[string]Client)
+	mu        sync.Mutex
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/user/messages", handleMessages)
+	http.HandleFunc("/user", handleRegistry)
 
-	http.HandleFunc("/user", handleMessages)
+	go func() {
+		for {
+			select {
+			case msg := <-broadcast:
+				sendBroadcast(msg)
+			default:
+				time.Sleep(500 * time.Millisecond)
+			}
+		}
+	}()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handleMessages(w http.ResponseWriter, r *http.Request) {
+func sendBroadcast(msg Message) {
+	fmt.Printf("TODO")
+}
+
+func handleRegistry(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
-	name := queryParams.Get("name")
+	clientId := queryParams.Get("name")
 
 	if r.Method != http.MethodPost {
 		http.Error(w, "only POST request allowed", http.StatusBadRequest)
 		return
 	}
+
+	if clientId == "" {
+		http.Error(w, "missing query parameter", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	mu.Lock()
+	if _, ok := clients[clientId]; ok {
+		http.Error(w, "client already defined", http.StatusBadRequest)
+		return
+	}
+	clients[clientId] = Client{string(body), clientId}
+	mu.Unlock()
+
+}
+
+func handleMessages(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	clientId := queryParams.Get("name")
+	if clientId == "" {
+		http.Error(w, "missing query parameter", http.StatusBadRequest)
+		return
+	}
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "only POST request allowed", http.StatusBadRequest)
+		return
+	}
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "error reading request body", http.StatusInternalServerError)
 		return
 	}
 	mu.Lock()
+	name := clients[clientId].Name
 	broadcast <- Message{name, string(body)}
 	mu.Unlock()
 }
